b52b5ca5c139e874ddbcbf5e6331813d89406d87
Сергей Белоусов <ss.belousov@yandex.ru>
2023-05-14 12:23:13 +0300

    Добавил вьюшки, сириализаторы и пермишн

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,20 +1,20 @@
 import datetime
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
-from reviews.models import Category, Genre, Title, User
+from reviews.models import Category, Genre, Title, User, Review, Comment
 from reviews.validators import validate_username
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ['name', 'slug']
+        fields = ["name", "slug"]
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['name', 'slug']
+        fields = ["name", "slug"]
 
 
 class CategoryField(serializers.SlugRelatedField):
@@ -30,63 +30,49 @@ class GenreField(serializers.SlugRelatedField):
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    category = CategoryField(slug_field='slug',
-                             queryset=Category.objects.all(),
-                             required=False
-                             )
-    genre = GenreField(slug_field='slug',
-                       queryset=Genre.objects.all(),
-                       many=True
-                       )
+    category = CategoryField(
+        slug_field="slug", queryset=Category.objects.all(), required=False
+    )
+    genre = GenreField(slug_field="slug", queryset=Genre.objects.all(), many=True)
     rating = serializers.FloatField(read_only=True)
 
     class Meta:
         model = Title
-        fields = '__all__'
+        fields = "__all__"
 
     def validate_year(self, value):
         year = datetime.date.today().year
         if year < value:
-            raise serializers.ValidationError(
-                'Год не может быть больше текущего'
-            )
+            raise serializers.ValidationError("Год не может быть больше текущего")
         return value
 
     def validate_genre(self, value):
         genre = Genre.objects.all()
         for item in value:
             if item not in genre:
-                raise serializers.ValidationError(
-                    'Жанра не существует'
-                )
+                raise serializers.ValidationError("Жанра не существует")
         return value
 
     def validate_category(self, value):
         category = Category.objects.all()
         if value not in category:
-            raise serializers.ValidationError('Категории не существует')
+            raise serializers.ValidationError("Категории не существует")
         return value
 
-      
+
 class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
         required=True,
         max_length=150,
-        validators=[
-            validate_username,
-            UniqueValidator(queryset=User.objects.all())
-        ]
+        validators=[validate_username, UniqueValidator(queryset=User.objects.all())],
     )
 
     class Meta:
         model = User
-        fields = (
-            'username', 'email', 'first_name', 'last_name', 'bio', 'role'
-        )
+        fields = ("username", "email", "first_name", "last_name", "bio", "role")
         validators = [
             UniqueTogetherValidator(
-                queryset=User.objects.all(),
-                fields=['username', 'email']
+                queryset=User.objects.all(), fields=["username", "email"]
             ),
         ]
 
@@ -94,16 +80,50 @@ class UserSerializer(serializers.ModelSerializer):
 class SignupSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True, max_length=254)
     username = serializers.CharField(
-        required=True,
-        max_length=150,
-        validators=[validate_username]
+        required=True, max_length=150, validators=[validate_username]
     )
 
 
 class TokenSerializer(serializers.Serializer):
     username = serializers.CharField(
-        required=True,
-        max_length=150,
-        validators=[validate_username]
+        required=True, max_length=150, validators=[validate_username]
     )
     confirmation_code = serializers.CharField(required=True, max_length=150)
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username",
+    )
+
+    class Meta:
+        model = Review
+        exclude = ("title",)
+        read_only_fields = ("pub_date",)
+
+    def validate(self, data):
+        if self.context["request"].method != "POST":
+            return data
+
+        title_id = self.context["view"].kwargs.get("title_id")
+        author = self.context["request"].user
+
+        if Review.objects.filter(author=author, title=title_id).exists():
+            raise serializers.ValidationError(
+                "Можно публиковать только один отзыв на произведение"
+            )
+
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username",
+    )
+
+    class Meta:
+        model = Comment
+        exclude = ("review",)
+        read_only_fields = ("review", "pub_date")
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,17 +1,36 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from api.views import CategoryViewSet, GenreViewSet, TitleViewSet, UserViewSet, signup, token
+from api.views import (
+    CategoryViewSet,
+    GenreViewSet,
+    TitleViewSet,
+    UserViewSet,
+    signup,
+    token,
+)
+from .views import (
+    ReviewViewSet,
+    CommentViewSet,
+)
 
 
-app_name = 'api'
+app_name = "api"
 v1_router = DefaultRouter()
-v1_router.register('genres', GenreViewSet, basename='genres')
-v1_router.register('categories', CategoryViewSet, basename='categories')
-v1_router.register('titles', TitleViewSet, basename='titles')
-v1_router.register('users', UserViewSet, basename='users')
+v1_router.register("genres", GenreViewSet, basename="genres")
+v1_router.register("categories", CategoryViewSet, basename="categories")
+v1_router.register("titles", TitleViewSet, basename="titles")
+v1_router.register("users", UserViewSet, basename="users")
+v1_router.register(
+    r"titles/(?P<title_id>\d+)/reviews", ReviewViewSet, basename="title-reviews"
+)
+v1_router.register(
+    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
+    CommentViewSet,
+    basename="review-comments",
+)
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
-    path('v1/auth/signup/', signup, name='signup'),
-    path('v1/auth/token/', token, name='login'),
+    path("v1/", include(v1_router.urls)),
+    path("v1/auth/signup/", signup, name="signup"),
+    path("v1/auth/token/", token, name="login"),
 ]
